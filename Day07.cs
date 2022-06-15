9a7af9bf6f22ed5edb2af3eb6bfb261986cecb9e
Chris Forseth (C4) <cforseth@straykitestudios.com>
2022-06-15 11:21:09 -0500

    Commenting day07 a bit

--- Day07.cs
+++ Day07.cs
@@ -49,28 +49,31 @@ namespace AoC2021
         public override string RunB()
         {
             // IntCodeMachine machine = new IntCodeMachine("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"); 
-            
             IntCodeMachine machine = new IntCodeMachine(IntCode); 
 
             int maxPower = 0; 
             foreach (int[] perm in Util.GetPermutations(5, 9)) {
+
+                // setup machines with their permuation code
                 IntCodeMachine[] amplifiers = new IntCodeMachine[5]; 
                 for (int i = 0; i < amplifiers.Length; ++i) {
                     amplifiers[i] = new IntCodeMachine( machine.IntCode ); 
                     amplifiers[i].EnqueueInput( perm[i] ); 
                 }
 
+                // set initial input for the first machine, and setup machine links
                 amplifiers[0].EnqueueInput( 0 ); 
                 for (int i = 0; i < amplifiers.Length; ++i) {
                     int next = (i + 1) % 5; 
                     amplifiers[i].PipeTo( amplifiers[next] ); 
                 }
 
+                // run the last amplifier until it halts
                 amplifiers[4].Run(); 
 
+                // get the final output and see if it is the max
                 int output;
                 amplifiers[4].DequeueOutput( out output ); 
-
                 maxPower = Math.Max(output, maxPower); 
             }
 
