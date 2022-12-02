80c98027822066cae4311fc8144d153d81895783
Christopher Forseth <christopher.forseth@gmail.com>
2022-12-01 23:17:48 -0600

    Absolute hack solution cause I don't want to think in cyclic groups tonight.

---
+++ 2022/Day02.cs
@@ -0,0 +1,108 @@
+﻿using AoC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC2022
+{
+   internal class Day02 : Day
+   {
+      private string InputFile = "2022/inputs/02.txt";
+      private List<string> Lines; 
+
+      //----------------------------------------------------------------------------------------------
+      public override void ParseInput()
+      {
+         Lines = Util.ReadFileToLines(InputFile);
+      }
+
+      //----------------------------------------------------------------------------------------------
+      public override string RunA()
+      {
+         int theirScore = 0; 
+         int myScore = 0; 
+         foreach (string line in Lines) {
+            int them = line[0] - 'A'; 
+            int me = line[2] - 'X'; 
+            
+            theirScore += them + 1; 
+            myScore += me + 1; 
+
+            int result = me - them; 
+            if (result < 0) {
+               result += 3; 
+            }
+
+            switch (result) {
+               case 0: 
+                  theirScore += 3; 
+                  myScore += 3; 
+                  break; 
+
+               case 1: 
+                  myScore += 6; 
+                  break; 
+
+               case 2: 
+                  theirScore += 6; 
+                  break; 
+
+               default: 
+                  break; 
+            }
+         }
+
+         return myScore.ToString(); 
+      }
+
+
+      //----------------------------------------------------------------------------------------------
+      public override string RunB()
+      {
+         int theirScore = 0; 
+         int myScore = 0; 
+         foreach (string line in Lines) {
+            int them = line[0] - 'A'; 
+            int desiredResult = line[2] - 'X'; 
+
+            desiredResult = desiredResult - 1; 
+            int me = them + desiredResult; 
+            if (me < 0) {
+               me += 3; 
+            } else if (me >= 3) {
+               me = 0; 
+            }
+            
+            theirScore += them + 1; 
+            myScore += me + 1; 
+
+            int result = desiredResult; 
+            if (result < 0) {
+               result += 3; 
+            }
+
+            switch (result) {
+               case 0: 
+                  theirScore += 3; 
+                  myScore += 3; 
+                  break; 
+
+               case 1: 
+                  myScore += 6; 
+                  break; 
+
+               case 2: 
+                  theirScore += 6; 
+                  break; 
+
+               default: 
+                  break; 
+            }
+         }
+
+         return myScore.ToString(); 
+      }
+   }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using AoC; 
 
-Day? day = Day.Create( "2022", args.FirstOrDefault(), typeof(AoC2022.Day01) );
+Day? day = Day.Create( "2022", args.FirstOrDefault(), typeof(AoC2022.Day02) );
 
 
 Util.WriteLine( @"[+white]                       /)"); 
