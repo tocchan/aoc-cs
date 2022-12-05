0ceb6cf7444a8959f520f2f1f70b2f1cfa7cf312
Christopher Forseth <christopher.forseth@gmail.com>
2022-12-04 23:19:06 -0600

    Day 05 solutions!

--- 2022/Day05.cs
+++ 2022/Day05.cs
@@ -11,24 +11,82 @@ namespace AoC2022
    {
       private string InputFile = "2022/inputs/05.txt";
       
-      private List<string> Lines = new(); 
+      private List<Stack<char>> Stacks = new(); 
+      private List<ivec3> Instructions = new(); 
 
       //----------------------------------------------------------------------------------------------
       public override void ParseInput()
       {
-         Lines = Util.ReadFileToLines(InputFile);
+         Stacks = new(); 
+         Instructions = new(); 
+         List<string> lines = Util.ReadFileToLines(InputFile);
+
+         for (int j = 0; j < 9; ++j) {
+            List<char> stack = new(); 
+            for (int i = 0; i < 8; ++i) {
+               char c = lines[i][4 * j + 1]; 
+               if (c != ' ') {
+                  stack.Add(c); 
+               }
+            }
+
+            stack.Reverse(); 
+            Stack<char> val = new(); 
+            for (int i = 0; i < stack.Count; ++i) {
+               val.Push(stack[i]); 
+            }
+
+            Stacks.Add(val); 
+         }
+
+         for (int i = 10; i < lines.Count; ++i) {
+            string[] words = lines[i].Split(' '); 
+            ivec3 val = new ivec3(int.Parse(words[1]), int.Parse(words[3]) - 1, int.Parse(words[5]) - 1); 
+            Instructions.Add(val); 
+         }
       }
 
       //----------------------------------------------------------------------------------------------
       public override string RunA()
       {
-         return ""; 
+         foreach (ivec3 i in Instructions) {
+            for (int c = 0; c < i.x; ++c) { 
+               char v = Stacks[i.y].Pop();
+               Stacks[i.z].Push(v); 
+            }
+         }
+
+         string result = ""; 
+         for (int i = 0; i < Stacks.Count; ++i) {
+            result += Stacks[i].Peek(); 
+         }
+         
+         return result; 
       }
 
       //----------------------------------------------------------------------------------------------
       public override string RunB()
       {
-         return ""; 
+         ParseInput(); // start fresh; 
+
+         Stack<char> hold = new(); 
+         foreach (ivec3 i in Instructions) {
+            
+            for (int c = 0; c < i.x; ++c) { 
+               hold.Push(Stacks[i.y].Pop());
+            }
+
+            while (hold.Count > 0) { 
+               Stacks[i.z].Push(hold.Pop()); 
+            }
+         }
+
+         string result = ""; 
+         for (int i = 0; i < Stacks.Count; ++i) {
+            result += Stacks[i].Peek(); 
+         }
+         
+         return result; 
       }
    }
 }
