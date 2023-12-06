4ea65b6259645636a8ae3c5dc4372bc6900fd5db
Christopher Forseth <christopher.forseth@gmail.com>
2023-12-05 23:23:36 -0600

    Day06, brought to you by brute force because I'm too lazy to solve a quadratic.

--- 2023/Day06.cs
+++ 2023/Day06.cs
@@ -11,23 +11,83 @@ namespace AoC2023
    {
       private string InputFile = "2023/inputs/06.txt";
 
+      int[] Times = new int[0]; 
+      int[] Distances = new int[0]; 
+
 
       //----------------------------------------------------------------------------------------------
       public override void ParseInput()
       {
          List<string> lines = Util.ReadFileToLines(InputFile);
+
+         Times = lines[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+         Distances = lines[1].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+      }
+
+      public int DetermineWinners(int ms, int distanceToBeat)
+      {
+         int count = 0; 
+         for (int i = 1; i < ms; ++i) {
+            int distance = (ms - i) * i; 
+            if (distance > distanceToBeat) {
+               ++count; 
+            }
+         }
+
+         return count; 
       }
 
       //----------------------------------------------------------------------------------------------
       public override string RunA()
       {
-         return "";
+         int total = 1; 
+         for (int i = 0; i < Times.Length; ++i) {
+            total *= DetermineWinners(Times[i], Distances[i]); 
+         }
+         return total.ToString(); 
+      }
+
+      public Int64 DetermineWinnersSmart(Int64 time, Int64 distance)
+      {
+         // determine first winner
+         Int64 startTime = 1; 
+         Int64 endTime = time; 
+
+         while (startTime < time) {
+            Int64 dist = (time - startTime) * startTime; 
+            if (dist > distance) {
+               break; 
+            }
+            ++startTime; 
+         }
+
+         while (endTime > 1) {
+            Int64 dist = (time - endTime) * endTime;
+            if (dist > distance) {
+               break; 
+            }
+            --endTime; 
+         }
+
+         // determine last winner
+         return (endTime - startTime) + 1; 
       }
 
       //----------------------------------------------------------------------------------------------
       public override string RunB()
       {
-         return "";
+         string timeString = ""; 
+         string distanceString = ""; 
+
+         for (int i = 0; i < Times.Length; ++i) {
+            timeString += Times[i].ToString(); 
+            distanceString += Distances[i].ToString(); 
+         }
+
+         Int64 time = Int64.Parse(timeString); 
+         Int64 distance = Int64.Parse(distanceString); 
+         
+         return DetermineWinnersSmart(time, distance).ToString(); 
       }
    }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using AoC; 
 
-Day? day = Day.Create( "2023", args.FirstOrDefault(), typeof(AoC2023.Day05) );
+Day? day = Day.Create( "2023", args.FirstOrDefault(), typeof(AoC2023.Day06) );
 
 
 Util.WriteLine( @"[+white]                       /)"); 
